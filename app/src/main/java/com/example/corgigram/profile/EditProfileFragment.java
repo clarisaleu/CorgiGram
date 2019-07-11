7a6b9590356b24d3f52ab1ecf6743aa07f874c71
Clarisa Leu <clarisaleu@gmail.com>
2019-07-11 03:54:30 -0700

    finallllyyy

--- app/src/main/java/com/example/corgigram/profile/EditProfileFragment.java
+++ app/src/main/java/com/example/corgigram/profile/EditProfileFragment.java
@@ -8,24 +8,43 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.example.corgigram.R;
+import com.example.corgigram.model.User;
 import com.parse.ParseFile;
 import com.parse.ParseUser;
+
+import org.w3c.dom.Text;
 
 public class EditProfileFragment extends Fragment  {
     private static final String TAG = "EditProfileFragment";
     private static Context mContext;
+
+    private static EditText username;
+    private static EditText name;
+    private static EditText email;
+    private static EditText website;
+    private static EditText phoneNumber;
+
+    private static final ParseUser user = ParseUser.getCurrentUser();
+
     ImageView mProfileImg;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_editprofile, container, false);
-        mProfileImg = (ImageView) view.findViewById(R.id.profile_image);
-        mContext = view.getContext();
+        mProfileImg = (ImageView) view.findViewById(R.id.profile_photo);
+        mContext = (Context) view.getContext();
+        username = (EditText) view.findViewById(R.id.username);
+        name = (EditText) view.findViewById(R.id.display_name);
+        email = (EditText)view.findViewById(R.id.email);
+        website = (EditText) view.findViewById(R.id.website);
+        phoneNumber = (EditText) view.findViewById(R.id.phoneNumber);
 
         // Get Image
         ParseUser user = ParseUser.getCurrentUser();
@@ -35,6 +54,15 @@ public class EditProfileFragment extends Fragment  {
             String imageUrl = img.getUrl();
             Glide.with(mContext).load(imageUrl).into(mProfileImg);
         }
+
+        String un = user.getUsername();
+        username.setText(un);
+
+        String mail = user.getEmail();
+        email.setText(mail);
+
+        String nameFull = user.getString("handle");
+        name.setText(nameFull);
 
         return  view;
     }
